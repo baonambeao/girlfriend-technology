f08b4c5626e06321515b613aa515b33182024b75
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-12-15 19:27:09 -0800

    global css vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-  renderers: ['@astrojs/renderer-react'],
   devOptions: {
     tailwindConfig: './tailwind.config.js',
   },
